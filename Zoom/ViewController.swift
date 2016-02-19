55062ca44ddd11909b4984a011a2445b0b3cf4e6
santosh1985 <santosh.pawar@macys.com>
2016-02-19 10:12:37 -0500

    Zoom image with magnifier
    Initial commit

--- Zoom/ViewController.swift
+++ Zoom/ViewController.swift
@@ -10,10 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+//    @IBOutlet weak var scrollView: UIScrollView!
+    
+    @IBOutlet var magnifyingGlassView: YPMagnifyingView!
+    @IBOutlet weak var myImageView: UIImageView!
+//    var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        myImageView.image = UIImage(named: "cutie")
+        let mag = YPMagnifyingGlass(frame:CGRectMake(magnifyingGlassView.frame.origin.x, magnifyingGlassView.frame.origin.y,100,100))
+        mag.scale = 2
+        self.magnifyingGlassView.magnifyingGlass = mag
+        //        scrollView.minimumZoomScale = 0.5
+//        scrollView.maximumZoomScale = 5.0
+//        scrollView.delegate = self
+//        imageView = UIImageView(image: UIImage(named: "cutie"))
+//        scrollView.addSubview(imageView)
     }
+    
+//    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+//        return imageView
+//    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
